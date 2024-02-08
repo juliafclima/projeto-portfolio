065e11534412a523bc15675e6630f4590a1d015c
Júlia Lima <juliafclima@hotmail.com>
2024-02-08 14:08:03 -0300

    feat: animacoes

--- js/animarTexto.js
+++ js/animarTexto.js
@@ -1,17 +1,50 @@
-var texto = "Dev Front-End";
+consoleText(['Hello, World!', 'Front-End Developer'], 'text',['black','black','black']);
 
-var intervalo = 50;
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ['#fff'];
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+  window.setInterval(function() {
 
-var index = 0;
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount)
+      window.setTimeout(function() {
+        var usedColor = colors.shift();
+        colors.push(usedColor);
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute('style', 'color:' + colors[0])
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function() {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x;
+    }
+  }, 120)
+  window.setInterval(function() {
+    if (visible === true) {
+      con.className = 'console-underscore hidden'
+      visible = false;
 
-function animarTexto() {
-   if (index < texto.length) {
-      document.getElementById("texto-maquina").innerHTML += texto.charAt(index);
-      index++;
-      setTimeout(animarTexto, intervalo);
-   }
+    } else {
+      con.className = 'console-underscore'
+
+      visible = true;
+    }
+  }, 400)
 }
-
-window.onload = function () {
-   animarTexto();
-};
